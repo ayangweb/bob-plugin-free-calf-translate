bd9e92557adcd24f874f930eafbaae06b660daf9
ayang <473033518@qq.com>
2023-03-07 14:54:43 +0800

    chore: add some dependencies and automatic publishing workflows

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ async function translate(query, completion) {
 				time: +new Date(),
 			},
 			header: {
-				Cookie: "Hm_lvt_b0622cd752283bf08fa70c966ac2c3cd=1677307242; Hm_lpvt_b0622cd752283bf08fa70c966ac2c3cd=1677307242; sajssdk_2015_cross_new_user=1; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22186874d6535169-089667c53ac5cb-1f525634-1930176-186874d65361a2d%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.google.com.hk%2F%22%7D%2C%22%24device_id%22%3A%22186874d6535169-089667c53ac5cb-1f525634-1930176-186874d65361a2d%22%7D",
+				Cookie: "sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22186a038e167410-021c9252a1823c-1f525634-1930176-186a038e1681b08%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%7D%2C%22%24device_id%22%3A%22186a038e167410-021c9252a1823c-1f525634-1930176-186a038e1681b08%22%7D; Hm_lvt_b0622cd752283bf08fa70c966ac2c3cd=1677725327,1677733929,1678171308; Hm_lpvt_b0622cd752283bf08fa70c966ac2c3cd=1678171308",
 				Origin: "https://niutrans.com",
 				Referer: "https://niutrans.com/",
 				"User-Agent":
